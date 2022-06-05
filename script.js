de1e5b9d1843a9de54793f5065b73ff2e6d7309b
krishna tiwari <99862810+ktiwari19@users.noreply.github.com>
2022-06-05 14:43:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,3 +38,18 @@ function setTheme(mode){
 
 	localStorage.setItem('theme', mode)
 }
+window.addEventListener("load", function() {
+  const form = document.getElementById('my-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Success!");
+    })
+  });
+});
