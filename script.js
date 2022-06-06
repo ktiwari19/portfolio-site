3dfa501f6108329d06a0a5714ea7450f9a50cce8
krishna tiwari <99862810+ktiwari19@users.noreply.github.com>
2022-06-06 12:43:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('Its working')
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyVL9XWkIVc2vi97gsv-bBQqGjxs7USome2eHzdwmA/dev'
-const form = document.forms['google-sheet']
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbyVL9XWkIVc2vi97gsv-bBQqGjxs7USome2eHzdwmA/dev'
+// const form = document.forms['google-sheet']
 
 let theme = localStorage.getItem('theme')
 
@@ -40,11 +40,3 @@ function setTheme(mode){
 
 	localStorage.setItem('theme', mode)
 }
-        
-          
-            form.addEventListener('submit', e => {
-              e.preventDefault()
-              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-                .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
-                .catch(error => console.error('Error!', error.message))
-            })
